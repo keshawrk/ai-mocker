61969172a23fc604bbb288044a586644545cd9a6
Keshaw <keshawrk94@gmail.com>
2025-03-31 12:23:06 +0530

    Initial setup

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2> Hello World</h2>
+      <Button cursor="pointer">Click Me</Button>
+    </div>
+    
+  );
+}
